3b779ace22ec249466021c57ecd918458c26b7e5
luckstrike <alexisgarc711@gmail.com>
2023-03-30 01:03:21 -0500

    Now launches bag and Reed's scanning node

--- ros2_ws/install/project3/share/project3/launch.py
+++ ros2_ws/install/project3/share/project3/launch.py
@@ -23,13 +23,16 @@ def generate_launch_description():
                             'bag_out',
                             default_value='bags/out/test')
 
-    # This launches a turtlesim node
-    # Should probably be a node dealing with /scan though
-    node = Node(package = 'turtlesim',
-                executable = 'turtlesim_node')
-
     # This starts playing the bag on launch
     play_bag = ExecuteProcess(cmd = ['ros2', 'bag', 'play', bag_in])
+    
+    # For bag recording, my guess is we need to pass in the argument
+    # Into the node we create and ros2 bag record bag_out in that node
+
+    # This launches a turtlesim node
+    # Should probably be a node dealing with /scan though
+    node = Node(package = 'project3',
+                executable = 'track')
 
     # This terminates the program once the bag is done playing
     event_handler = OnProcessExit(target_action = play_bag,
