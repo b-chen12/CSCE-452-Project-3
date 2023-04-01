a77d53025339cf4e84d0ad00423c2fef444720d7
rjbilledo <70819437+rjbilledo@users.noreply.github.com>
2023-03-31 20:49:44 -0500

    Update setup.py

--- ros2_ws/src/project3/setup.py
+++ ros2_ws/src/project3/setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 import glob
-
 package_name = 'project3'
 
 setup(
@@ -8,21 +7,21 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, glob.glob('launch/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='vboxuser',
-    maintainer_email='alexisgarc711@gmail.com',
+    maintainer='osboxes',
+    maintainer_email='osboxes@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'track = project3.track:main',
+            'count = project3.count:main',
         ],
     },
 )
