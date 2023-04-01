cf989e53fd1b03512a1532f203eb321a36f916ea
rjbilledo <70819437+rjbilledo@users.noreply.github.com>
2023-03-31 20:52:01 -0500

    Update track.py

--- ros2_ws/src/project3/project3/track.py
+++ ros2_ws/src/project3/project3/track.py
@@ -194,112 +194,19 @@ class ScanSubscriber(Node):
         
         return clusters
 
-class TopicPublisher(Node):
-    # This node publishes onto the three separate topics
-    def __init__(self):
-        super().__init__('ScanSubscriber')
-        self.cluster_receiver = self.create_subscription(Float32MultiArray, '/clusters', self.listener_callback, 10)
-        self.person_count_curr = self.create_publisher(Int64, '/person_count_current', 10)
-        self.person_count_tot = self.create_publisher(Int64, '/person_count_total', 10)
-    
-    def listener_callback(self, msg):
-        self.get_logger().info('Here!')
-        self.get_logger().info('I heard: "%s"' % msg.data)
 
-    def msg_to_clusters(self, msg):
-        clusters = []
-        temp_list = []
-
-        # Removing the 100 and sorting coordinates
-        for val in msg:
-            if val == 100.00:
-                if temp_list:
-                    clusters.append(temp_list)
-                    temp_list = []
-            else:
-                temp_list.append(val)
-
-        if temp_list:
-            clusters.append(temp_list)
-
-        coords = []
-
-        for i in range(len(clusters)):
-            coords_row = []
-            for j in range(0, len(clusters[i]), 2):
-                coords_row.append((clusters[i][j], clusters[i][j + 1]))
-
-            coords.append(coords_row)
-
-        return coords
-
-
-    def wall_filter(self, clusters, msg):
-        # For testing rn, to see where the clusters are at, making a PointCloud
-        pointcloud_msg = PointCloud()
-        pointcloud_msg.header = msg.header
-        # First iteration, get the walls of the map
-        if not self.walls:
-            # Finding the moving average
-            for cluster in clusters:
-                xa = sum([point[0] for point in cluster])/len(cluster)
-                ya = sum([point[1] for point in cluster])/len(cluster)
- 
-                self.walls.append([xa,ya])
-
-        # Future iterations, for each cluster check if it is within range of a wall, if it is break; if it is not at it to the Point Cloud
-        else:
-            for cluster in clusters:
-                # Finding the moving average
-                xa = sum([point[0] for point in cluster])/len(cluster)
-                ya = sum([point[1] for point in cluster])/len(cluster)
-                z=0.0
-                isWall = False
-                for wall in self.walls:
-                    # Checking if within range of wall
-                    if(self.euclidean_distance((xa,ya), wall) <= 0.8):
-                        isWall = True
-                        #self.walls.append((xa,ya))
-                        # wall[0]=(wall[0]+xa)/2
-                        # wall[1]=(wall[1]+ya)/2 
-                        break
-                if(isWall == False):
-
-                    # Storing people; go through stored people to see if current person is within range of someone, this would mean they are the same person
-                    isNew = True
-                    for i, center in enumerate(self.total):
-                        if(self.euclidean_distance(center,[xa,ya])<=1):
-                            isNew = False
-                            self.total[i] = [xa,ya]
-                            break
-                    # New person, add to list
-                    if(isNew == True):
-                        self.total.append([xa,ya])
-                        
-
-                    pointcloud_msg.points.append(Point32(x=xa, 
-                                                            y=ya, 
-                                                            z=0.0))
-        # Adds in all of the points to the PointCloud message
-        total = Int64()
-        total.data = len(self.total)
-        self.person_location.publish(pointcloud_msg)
-        self.person_count_tot.publish(total)
-        self.get_logger().info('TOTAL: "%s"' % total)
-
-        #self.get_logger().info('Current TOTAL: ' "%s" % len(pointcloud_msg.points))
 
 def main(args=None):
     rclpy.init(args=args)
     scan_sub = ScanSubscriber()
-    topic_pub = TopicPublisher()
+
 
     rclpy.spin(scan_sub)
-    rclpy.spin(topic_pub)
+
 
     # Destroy node
     scan_sub.destroy_node()
-    topic_pub.destroy_node()
+
 
     rclpy.shutdown()
 
